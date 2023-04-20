4facbc181a600502b5b2a44b7abe87af59355be5
Grace Pu <33011813+Goldengold123@users.noreply.github.com>
2023-04-20 15:10:50 -0400

    Update

--- scripts/collapse.js
+++ scripts/collapse.js
@@ -1,8 +1,11 @@
 var c = document.getElementsByClassName("collapse");
+var i;
 
-for (var i = 0; i < c.length; i++) {
+for (i = 0; i < c.length; i++) {
   c[i].addEventListener("click", function () {
-    this.classList.toggle("clicked");
-    c[i].nextElementSibling.classList.toggle("show");
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    content.style.display =
+      content.style.display === "block" ? "none" : "block";
   });
 }
